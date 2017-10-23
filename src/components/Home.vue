3099fc209841b7329d3c08a5f6c73af0cfb03118
Maxwell Stern <Stern@Maxwells-MacBook-Pro.local>
2017-10-23 14:24:40 -0500

    Homepage carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,55 @@
 <template lang="html">
-  <div>
-    <p>
-      The Homepage
-    </p>
-  </div>
+  <v-container>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item v-for="meetup in meetups" :key="meetup.id" :src="meetup.imageUrl">
+            <div class="title">
+              {{meetup.title}}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our awesome meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
 
 <script>
 export default {
+  data () {
+    return {
+      meetups: [
+        { imageUrl: 'https://media.timeout.com/images/103678916/image.jpg', id: 'asdadasdasdasdas', title: 'Meetup in NYC' },
+        { imageUrl: 'http://wcbct2019.org/wp-content/uploads/2016/11/AdobeStock_127125938-small-uai-1032x616.jpeg', id: 'asdadasdfeeefeas', title: 'Meetup in Berlin' },
+        { imageUrl: 'http://cdn.funcheap.com/wp-content/uploads/2013/09/cityscape-of-san-francisco-from-twin-peaks-california1-563x422.jpg', id: 'asdadasdasdasdas', title: 'Meetup in San Francisco' }
+      ]
+    }
+  }
 }
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0,0,0,0.5);
+    color:white;
+    font-size:2em;
+    padding: 20px;
+  }
 </style>
